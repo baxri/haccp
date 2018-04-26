0ae993c315d6caff8f15cbc7eea813d53a92b82a
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2018-04-26 15:55:47 +0400

    Link realm databse to react native app

--- android/app/src/main/java/com/haccp/MainApplication.java
+++ android/app/src/main/java/com/haccp/MainApplication.java
@@ -3,6 +3,10 @@ package com.haccp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
+import io.realm.react.RealmReactPackage;
+import io.realm.react.RealmReactPackage;
+import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -10,6 +14,7 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+import io.realm.react.RealmReactPackage; // add this import
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RealmReactPackage()           
       );
     }
 
