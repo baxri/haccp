aba236b23398f379f3067e9dc7f17527e9f0b6e6
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-05-13 00:00:59 +0400

    --

--- src/screens/front/archive/list.js
+++ src/screens/front/archive/list.js
@@ -106,7 +106,6 @@ export class ArchiveListScreen extends React.Component {
                     <Row>
                         <Col style={{ backgroundColor: 'black' }}>
                             <Content>
-
                                 {!this.state.controles.length && <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingTop: 100, }}>
                                     <Icon name='camera' fontSize={50} size={50} style={{ color: 'white', fontSize: 100, }} />
                                     <Text style={{ color: 'white', fontSize: 25, }} >There is no Pictures</Text>
@@ -116,8 +115,7 @@ export class ArchiveListScreen extends React.Component {
                                     style={{
                                         flexDirection: 'row',
                                         flexWrap: 'wrap',
-                                    }}
-                                >
+                                    }}>
 
                                     {this.state.pictures.map((row, index) => {
                                         return <Button
