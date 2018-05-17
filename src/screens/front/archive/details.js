f9758663eed116364d5510958b971d18b7ecc005
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2018-05-17 12:59:29 +0400

    --

--- src/screens/front/archive/details.js
+++ src/screens/front/archive/details.js
@@ -29,8 +29,40 @@ export class ArchiveDetailsScreen extends React.Component {
 
         this.state = {
             item: this.props.navigation.state.params.controle,
+            equipments: [],
         };
+
+        this._bootstrapAsync();
     }
+
+    _bootstrapAsync = async () => {
+
+    };
+
+    _parseEquipments(equipments) {
+
+        let ret = [];
+
+        this.state.item.equipments.map(equipment => {
+
+            let slice = equipment.split(":");
+
+            let obj = {
+                id: slice[0],
+                name: slice[1],
+                value: slice[2],
+            };
+
+            ret.push(obj);
+        });
+
+        this.setState({ equipments: ret });
+
+    }
+
+    componentDidMount() {
+        this._parseEquipments();
+    };
 
     render() {
         return (
@@ -52,27 +84,34 @@ export class ArchiveDetailsScreen extends React.Component {
                     </View>
 
                     <View style={{ paddingTop: 20, }}>
-                        <H1 style={{ marginBottom: 25, }}>{Strings.PRODUCT}: {this.state.item.produit}</H1>
+                        {this.state.item.type == 0 && <H1 style={{ marginBottom: 25, }}>{Strings.PRODUCT}: {this.state.item.produit}</H1>}
 
-                        {this.state.item.confirmed == 1 && <View style={{ flexDirection: 'row' }}>
+                        {this.state.item.confirmed == 1 && <View style={{ flexDirection: 'row', marginBottom: 20, }}>
                             <Button iconLeft success>
                                 <Icon name='checkmark' />
                                 <Text>{Strings.CONFIRMED}</Text>
                             </Button>
                         </View>}
-                        {this.state.item.confirmed == 0 && <View><Button iconLeft danger>
+                        {this.state.item.confirmed == 0 && <View style={{ marginBottom: 20, }}><Button iconLeft danger>
                             <Icon name='close' />
                             <Text>{Strings.NOT_CONFIRMED}</Text>
                         </Button></View>}
 
-                        <H3 style={{ marginTop: 20, }}>{Strings.FOURNISER}: {this.state.item.fourniser}</H3>
-                        <H3>{Strings.DUBL}: {this.state.item.dubl}</H3>
-                        <H3>{Strings.ASPECT}: {this.state.item.aspect}</H3>
-                        <H3>{Strings.DUPRODUIT}T: {this.state.item.du_produit}</H3>
-                        <H3>{Strings.EMBALAGE_INTATC}: {this.state.item.intact}</H3>
-                        <H3>{Strings.ETIQUTAGE_CONF}: {this.state.item.conforme}</H3>
+                        {this.state.item.type == 0 && <H3>{Strings.FOURNISER}: {this.state.item.fourniser}</H3>}
+                        {this.state.item.type == 0 && <H3>{Strings.DUBL}: {this.state.item.dubl}</H3>}
+                        {this.state.item.type == 0 && <H3>{Strings.ASPECT}: {this.state.item.aspect}</H3>}
+                        {this.state.item.type == 0 && <H3>{Strings.DUPRODUIT}T: {this.state.item.du_produit}</H3>}
+                        {this.state.item.type == 0 && <H3>{Strings.EMBALAGE_INTATC}: {this.state.item.intact}</H3>}
+                        {this.state.item.type == 0 && <H3>{Strings.ETIQUTAGE_CONF}: {this.state.item.conforme}</H3>}
+
                         <H3>{Strings.AUTRES}: {this.state.item.autres}</H3>
                         <H3>{Strings.ACTION_CORECTIVES}: {this.state.item.actions}</H3>
+
+                        {this.state.equipments.length > 0 && <View style={{ marginTop: 20, marginBottom: 20, }}>
+                            {this.state.equipments.map((row) => {
+                                return <H3>{row.name}: {row.value}</H3>;
+                            })}
+                        </View>}
 
                         <H3>{Strings.DATE}: {this.state.item.date}</H3>
                         <H3>{Strings.DATETIME}: {this.state.item.created_at.toString()}</H3>
