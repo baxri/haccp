c9fc5e0107a870f5fde62165b22672083c21952b
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-05-30 02:26:48 +0400

    --

--- src/screens/front/nonconflist/details.js
+++ src/screens/front/nonconflist/details.js
@@ -9,11 +9,13 @@ import {
     ToastAndroid,
 
 } from 'react-native';
-import { Container, Header, Content, Button, Text, Picker, H2, Icon, FooterTab, Footer, H1, H3 } from 'native-base';
+import { Container, Header, Content, Button, Text, Picker, H2, Icon, FooterTab, Footer, H1, H3, Card, Body, Left, Right, Title, CardItem } from 'native-base';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import { NoBackButton, LogoTitle, Menu } from '../../../components/header';
 import Strings from '../../../language/fr';
 import { reverseFormat } from '../../../utilities/index';
+import { FilePicturePath, writePicture } from '../../../utilities/index';
+
 
 
 export class NonConformeDetailsScreen extends React.Component {
@@ -69,62 +71,159 @@ export class NonConformeDetailsScreen extends React.Component {
     render() {
         return (
             <Container>
-                <View style={{ flex: 1, flexDirection: 'row' }}>
+                <Content padder>
+                    <Card>
+                        <CardItem header bordered>
+                            {this.state.item.type == 0 && <Text>{Strings.PRODUCT}: {this.state.item.produit}</Text>}
+                            {this.state.item.type == 1 && <Text>{Strings.CONTROLE_FROID}</Text>}
+                            {this.state.item.type == 2 && <Text>{Strings.PRODUCT}: {this.state.item.produit}</Text>}
+                        </CardItem>
+                        <CardItem bordered>
+                            <Body>
+                                <View style={{ padding: 10, flexDirection: 'row' }}>
+                                    {this.state.item.source != '' && <View style={{ backgroundColor: 'white', margin: 10, }}><Image
+                                        resizeMode={'cover'}
+                                        style={{ width: 70, height: 70, borderRadius: 100, }}
+                                        source={{ uri: FilePicturePath() + this.state.item.source }}
+                                    /></View>}
 
-                    <View style={{ padding: 10, }}>
-                        {this.state.item.source != '' && <View style={{ backgroundColor: 'white', margin: 10, }}><Image
-                            resizeMode={'cover'}
-                            style={{ width: 200, height: 200, }}
-                            source={{ uri: this.state.item.source }}
-                        /></View>}
+                                    {this.state.item.signature != '' && <View style={{ backgroundColor: 'white', margin: 10, borderRadius: 100, borderWidth: 1 }}><Image
+                                        resizeMode={'cover'}
+                                        style={{ width: 70, height: 70, borderRadius: 100, borderWidth: 1, }}
+                                        source={{ uri: FilePicturePath() + this.state.item.signature }}
+                                    /></View>}
+                                </View>
+                            </Body>
+                        </CardItem>
 
-                        {this.state.item.signature != '' && <View style={{ backgroundColor: 'white', borderWidth: 1, margin: 10, }}><Image
-                            resizeMode={'cover'}
-                            style={{ width: 200, height: 200, }}
-                            source={{ uri: this.state.item.signature }}
-                        /></View>}
-                    </View>
+                        {this.state.item.type == 0 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.FOURNISER}: {this.state.item.fourniser}</Text>
+                            </Body>
+                        </CardItem>}
 
-                    <View style={{ paddingTop: 20, }}>
-                        {this.state.item.type == 0 && <H1 style={{ marginBottom: 25, }}>{Strings.PRODUCT}: {this.state.item.produit}</H1>}
+                        {this.state.item.type == 0 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.DUBL}: {this.state.item.dubl}</Text>
+                            </Body>
+                        </CardItem>}
 
-                        {this.state.item.confirmed == 1 && <View style={{ flexDirection: 'row', marginBottom: 20, }}>
-                            <Button iconLeft success>
-                                <Icon name='checkmark' />
-                                <Text>{Strings.CONFIRMED}</Text>
-                            </Button>
-                        </View>}
-                        {this.state.item.confirmed == 0 && <View style={{ marginBottom: 20, }}><Button iconLeft danger>
-                            <Icon name='close' />
-                            <Text>{Strings.NOT_CONFIRMED}</Text>
-                        </Button></View>}
+                        {this.state.item.type == 0 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.ASPECT}: {this.state.item.aspect == 0 ? Strings.BON : Strings.MAUVAIS}</Text>
+                            </Body>
+                        </CardItem>}
 
-                        {this.state.item.type == 0 && <H3>{Strings.FOURNISER}: {this.state.item.fourniser}</H3>}
-                        {this.state.item.type == 0 && <H3>{Strings.DUBL}: {this.state.item.dubl}</H3>}
-                        {this.state.item.type == 0 && <H3>{Strings.ASPECT}: {this.state.item.aspect}</H3>}
-                        {this.state.item.type == 0 && <H3>{Strings.DUPRODUIT}T: {this.state.item.du_produit}</H3>}
-                        {this.state.item.type == 0 && <H3>{Strings.EMBALAGE_INTATC}: {this.state.item.intact}</H3>}
-                        {this.state.item.type == 0 && <H3>{Strings.ETIQUTAGE_CONF}: {this.state.item.conforme}</H3>}
+                        {this.state.item.type == 0 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.DUPRODUIT}: {this.state.item.du_produit}</Text>
+                            </Body>
+                        </CardItem>}
 
-                        <H3>{Strings.AUTRES}: {this.state.item.autres}</H3>
-                        <H3>{Strings.ACTION_CORECTIVES}: {this.state.item.actions}</H3>
 
-                        {this.state.equipments.length > 0 && <View style={{ marginTop: 20, marginBottom: 20, }}>
-                            {this.state.equipments.map((row) => {
-                                return <View style={{ marginBottom: 20, }}>
-                                    <H3 style={{ marginBottom: 10, }}>{row.name}</H3>
-                                    {row.value.split(",").map(val => {
-                                        return <Text>{val}</Text>
+                        {this.state.item.type == 0 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.EMBALAGE_INTATC}: {this.state.item.intact == 0 ? Strings.NO : Strings.YES}</Text>
+                            </Body>
+                        </CardItem>}
+
+
+                        {this.state.item.type == 0 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.ETIQUTAGE_CONF}: {this.state.item.conforme == 0 ? Strings.NO : Strings.YES}</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {this.state.item.type == 0 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.AUTRES}: {this.state.item.autres}</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {this.state.item.type == 0 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.ACTION_CORECTIVES}: {this.state.item.actions}</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {this.state.item.type == 1 && <CardItem bordered>
+                            <Body>
+                                <View style={{ marginTop: 20, marginBottom: 20, }}>
+                                    {this.state.equipments.map((row) => {
+                                        return <View style={{ marginBottom: 20, }}>
+                                            <H3 style={{ marginBottom: 10, }}>{row.name}</H3>
+                                            {row.value.split(",").map(val => {
+                                                return <Text>{Strings.TEMPERATURE}: {val}°</Text>
+                                            })}
+                                        </View>;
                                     })}
-                                </View>;
-                            })}
-                        </View>}
+                                </View>
+                            </Body>
+                        </CardItem>}
 
-                        {this.state.item.type == 2 && <H3>{Strings.TRAITMENT_DATE}: {this.state.item.traitment_date.toString()}</H3>}
-                        <H3>{Strings.DATE}: {this.state.item.date}</H3>
-                        <H3>{Strings.DATETIME}: {this.state.item.created_at.toString()}</H3>
-                    </View>
-                </View>
+                        {this.state.item.type == 1 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.AUTRES}: {this.state.item.autres}°</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {this.state.item.type == 1 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.AUTRES_CORECTIVES}: {this.state.item.actions}</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {this.state.item.type == 2 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.QUANTITY}: {this.state.item.quantity}</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {this.state.item.type == 2 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.VALORISATION}: {this.state.item.valorisation} ‎€</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {this.state.item.type == 2 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.CAUSES}: {this.state.item.causes}‎</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {this.state.item.type == 2 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.DEVENIR}: {this.state.item.devenir}‎</Text>
+                            </Body>
+                        </CardItem>}
+
+
+                        {this.state.item.type == 2 && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.TRAITMENT_DATE}: {reverseFormat(this.state.item.traitment_date.toISOString().substring(0, 10))}</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {true && <CardItem bordered>
+                            <Body>
+                                <Text>{Strings.DATETIME}: {reverseFormat(this.state.item.created_at.toISOString().substring(0, 10))}</Text>
+                            </Body>
+                        </CardItem>}
+
+                        {/* <CardItem bordered>
+                        <Body>
+                            <Text>
+                                Get on the mobile fast track with NativeBase, the
+                                fastest-growing platform
+                                and tool set for iOS and Android development.
+                  </Text>
+                        </Body>
+                    </CardItem>
+                    <CardItem footer bordered>
+                        <Text>GeekyAnts</Text>
+                    </CardItem> */}
+                    </Card>
+                </Content>
             </Container>
         );
     }
