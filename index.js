1c8752c5d66ba36079b964e808b80d27b60f1bb0
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2018-07-10 13:16:10 +0400

    --

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-import { Sentry } from 'react-native-sentry';
-Sentry.config('https://65085d84c4984fafa1700fa8b2d22e51@sentry.io/1222245').install();
+// import { Sentry } from 'react-native-sentry';
+// Sentry.config('https://65085d84c4984fafa1700fa8b2d22e51@sentry.io/1222245').install();
 
 AppRegistry.registerComponent('haccp', () => App);
